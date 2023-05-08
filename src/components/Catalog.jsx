c726cecfbc40490fb89b4aef68e3ac01bcb62f19
NikoFX <nikoofx@gmail.com>
2023-05-08 15:52:56 +0400

    fixed: some issues

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -1,26 +1,34 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Catalog() {
+
+    const navigate = useNavigate()
+
+    const handleShowAll = (e) => {
+        navigate('listofcoins')
+    }
+
     return (
         <div className="coin-catalog">
             <div className="c-coin-card">
                 <div className="c-coin-card__info">
                     <h3 className="c-coin-card__title">Bullion coins</h3>
-                    <button id='c-coin-card__btn'>Show all</button>
+                    <button id='c-coin-card__btn' onClick={handleShowAll}>Show all</button>
                 </div>
                 <img src="https://coin-app-server-api.vercel.app/src/img/South Vietnamese Dong_1.png" alt="" className="c-coin-card__img" />
             </div>
             <div className="c-coin-card">
                 <div className="c-coin-card__info">
                     <h3 className="c-coin-card__title">Bullion coins</h3>
-                    <button id='c-coin-card__btn'>Show all</button>
+                    <button id='c-coin-card__btn' onClick={handleShowAll}>Show all</button>
                 </div>
                 <img src="https://coin-app-server-api.vercel.app/src/img/coin.png" alt="" className="c-coin-card__img" />
             </div>
             <div className="c-coin-card">
                 <div className="c-coin-card__info">
                     <h3 className="c-coin-card__title">Bullion coins</h3>
-                    <button id='c-coin-card__btn'>Show all</button>
+                    <button id='c-coin-card__btn' onClick={handleShowAll}>Show all</button>
                 </div>
                 <img src="https://coin-app-server-api.vercel.app/src/img/Looney_2.png" alt="" className="c-coin-card__img" />
             </div>
